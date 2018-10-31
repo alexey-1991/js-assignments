70ea718022af95af6ad73037f32ab516780952a8
Aliaksei_Yakubuk <Aliaksei_Yakubuk@epam.com>
2018-10-31 12:54:22 +0300

    feat: getRect String

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -231,8 +231,70 @@ export function extractEmails(str) {
  *
  */
 export function getRectangleString(width, height) {
-  /* implement your code here */
-  throw new Error('Not implemented');
+  let result="";
+
+  for (let row=1;row<=height;row++){
+      
+      switch (row){
+          case 1:
+
+              for (let col=1; col<=width; col++){
+                  switch (col){
+                      case 1:
+                          result=result+"┌";
+                          break;
+                      case width:
+                          result=result+"┐\n";
+                          break;
+                      default:
+                          result=result+"─";
+                          break;
+
+                  }
+              }
+
+              break;
+
+          case height:
+
+              for (let col=1; col<=width; col++){
+                  switch (col){
+                      case 1:
+                          
+                          result=result+"└";
+                          break;
+                      case width:
+                          result=result+"┘\n";
+                          break;
+                      default:
+                          result=result+"─";
+                          break;
+                  }
+              }
+              break;
+
+          default:
+
+              for (let col=1; col<=width; col++){
+                  switch (col){
+                      case 1:
+                          result=result+"│";
+                          break;
+                      case width:
+                          result=result+"│\n";
+                          break;
+                      default:
+                          result=result+" ";
+                          break;
+                  }
+              }
+              break;
+
+      }
+
+  }
+
+  return result;
 }
 
 

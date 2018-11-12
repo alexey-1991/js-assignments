bec4b2d7f38a64388d2b156e04d8c31a043efd50
Aliaksei_Yakubuk <Aliaksei_Yakubuk@epam.com>
2018-11-12 17:28:39 +0300

    last task remains

--- myfolder/run.js
+++ myfolder/run.js
@@ -1,146 +1,67 @@
-function* expandBraces(str) {
+const str='https://en.wikipedia.org/wiki/Percent-encoding#Types_of_URI_characters';
 
-    const reg = /{[^{}]*({[^{}]*})?[^{}]*}/g;
-    if(!reg.test(str)) {
-        yield str;
-        return;
-    } 
 
-    const obj=createGroup2(str);
-    const template = obj.str;
-    const groupArr=obj.group;
-    let resultArr=[template];
 
-    const regTemplate=/[#]br\d[#]/g
-    let cond=resultArr.find(elem=>regTemplate.test(elem));
 
+
+
+let newStr='';
+
+for (let i=0;i<str.length;i++){
+    const prevelem=str[i-1]||'1';
+    const elem=str[i];
+    const nextelem=str[i+1]||'1';
+
+    const code1=(elem.codePointAt(0)>=100);
+    const code2=(prevelem.codePointAt(0)>=100);
+    const code3=(nextelem.codePointAt(0)>=100);
+
+
+
+    if (code1 && code2 && code3){
+
+        newStr+=`${elem.codePointAt(0)}`
+        continue;
+    }
     
-    while (cond){
-        groupArr.forEach((group,i)=>{
+    newStr+=elem;
+}
 
-            const name=`#br${i}#`;
-            resultArr=applyGroup2(name,group,resultArr);
-            
-        })  
-        cond=resultArr.find(elem=>regTemplate.test(elem));
+console.log(newStr);
+//String.fromCodePoint
+//str.codePointAt
+
+let encodedStr='';
+
+for (let i=0;i<newStr.length;i++){
+    const testString=newStr.slice(i,i+5);
+    const reg=/\d{5}/
+    if (reg.test(testString)){
+        encodedStr+=String.fromCodePoint(+testString);
+        i=i+4
+        continue;
     }
-    resultArr=removeRepeats(resultArr);
-
-    for (const comb of resultArr){
-        yield comb;
-    }
-
+    encodedStr+=newStr[i];
 }
 
+console.log(str)
+console.log(encodedStr)
 
-function createGroup2(str){
-    //@param {string} input string
-    //@return {object} //{group:array,str:string}
-    //group: array for example [ [ 'Downloads', 'Picture' ], [ 'jpg', 'gif', 'png' ] ]
-    //str: template string for example 'Itbr2ebr1, please.'
-    const reg = /{[^{}]*}/g;
-    const group=[];
-    let index=0;
-    
-    while (reg.test(str)){
-        const editable = str.match(reg);
-        editable.forEach(elem=>{
-            const name=`#br${group.length}#`;
-            str=str.replace(elem,name);
-            group.push(elem.slice(1,-1).split(','))
-        })
-    }
-
-    return {group:group,str:str};
-}
-function applyGroup2(str,arr,resultArray){
-
-    //@param {str} name of string in template string, example: 'br1','br0' ... 
-    //@param {array} array of possible strings
-    //@param {array} result array of string
- 
-    //@return {array} 
-
-    const newResultArray=[];
-
-    for (const item of resultArray){
-        for (const elem of arr){
-            newResultArray.push(item.replace(str,elem)); 
-        }
-    }
-
-    return (newResultArray.length)? newResultArray : resultArray;
-    
-}
-function removeRepeats(array){
-    const obj={};
-    array.forEach(elem=>obj[elem]=1);
-    return Object.keys(obj);
-}
-
-
-
-
-const str = '~/{Downloads,Pictures}/*.{jpg,gif,png}';
-const str2 = 'It{{em,alic}iz,erat}e{d,}, please.';
-const str3 = 'thumbnail.{png,jp{e,}g}';
-const str4 = '~/{Downloads,Pictures}/*.bbf';
-const str5 = '~/5641';
-
-
-for (let elem of expandBraces(str)) {
-    console.log(elem)
-}
-
-
-// console.log(createTemplate(str3));
-// console.log(applyGroup2(str3))
-
-
-//--------------------------------------------
-// function createTemplate(str){
-//     //@param {string} input string
-//     //@return {str}  template string '~/br0/*.br1'
-//     const reg = /{[^{}]*({[^{}]*})?[^{}]*}/g;
-//     const editable = str.match(reg);
-//     let template = str;
-//     editable.forEach((elem, i) => {
-//         const reg1 = new RegExp(elem);
-//         const name = `br${i}`;
-//         template = template.replace(reg1, name);
-//     });
-//     return template;
-// }
-
-// function applyGroup(str,arr,resultArray){
-
-//     //@param {str} name of string in template string, example: 'br1','br0' ... 
-//     //@param {array} array of possible strings
-//     //@param {array} result array of string
- 
-//     //@return {array} 
-//     const numOfRepeats=resultArray.length/arr.length;
-//     let k=0;
-
-//     for (let i=0;i<numOfRepeats;i++){
-
-//         for (let j=0;j<arr.length;j++){
-//             const elem=arr[j];
-//             resultArray[k]=resultArray[k].replace(str,elem)
-//             k++;
-//         }   
+// for (let i=0;i<encodedStr.length;i++){
+//     const testString=newStr.slice(i,i+3);
+//     const reg=/\d{3}/
+//     if (reg.test(testString)){
+//         encodedStr+=String.fromCodePoint(+testString);
+//         i=i+2
+//         continue;
 //     }
+//     encodedStr+=encodedStr[i];
+// }
 
-//     return resultArray; 
-// }
-// function createResArray(groupArr,template){
-//     //@param {str} template  string
-//     //@param {array} array of possible strings
-//     //@return {array} 
-//     const dimensionResArray=groupArr.reduce((acc,elem)=>acc*elem.length,1);
-//     const resultArray=[];
-//     for (let i=0;i<dimensionResArray;i++){
-//         resultArray.push(template)
-//     }
-//     return resultArray;
-// }
+
+
+
+
+
+
+
